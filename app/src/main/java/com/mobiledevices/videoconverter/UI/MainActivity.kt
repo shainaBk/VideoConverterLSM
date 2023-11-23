b0458f79d6042723396bed52bbb6c1d3ca8280aa
Shaïna BAKILI <shaina3322@gmail.com>
2023-11-24 00:32:39 +0100

    cleanup mainActivity

--- app/src/main/java/com/mobiledevices/videoconverter/UI/MainActivity.kt
+++ app/src/main/java/com/mobiledevices/videoconverter/UI/MainActivity.kt
@@ -13,37 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-        /**PART RECUP VIDEOS**/
-        val result = MusicService.getMusics("Brent Fayaz")
-        //val result = pythonScript.callAttr("getMusics","Brent Fayaz").toString()
-        // Afficher dans les logs
-        if (result.isNotEmpty()) {
-            for (i in 0 until result.size) {
-                Log.d("PythonTest", "Thumbnail URL of Music $i => ${result[i].title}")
-                if (i == 4) break // Arrêtez la boucle après avoir traité les 5 premiers éléments
-            }
-        } else {
-            Log.d("PythonTest", "Pas de résultats")
-        }
-
-
-        //TODO: FIX THAT PART!!!
-        /*
-        /**TEST CHAQOPY**/
-        val python = Python.getInstance()
-        val pythonScript = python.getModule("youtubeToMp3")
-        /**PART DOWNLOAD SONG**/
-        val downloadPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath
-        //val internalStoragePath = getFilesDir()
-        Log.d("PythonTest2",downloadPath)
-
-        pythonScript.callAttr("create_file",downloadPath,"fululu.txt","testSmèere")
-        //pythonScript.callAttr("downloadMusics",result,internalStoragePath)
-        Log.d("PythonTest2","Download DONE")*/
-
     }
 
 }
