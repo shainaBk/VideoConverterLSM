fbaab49d7d46b47b60c2f776a757dae07a8bc416
Mathious6 <mathious61@gmail.com>
2023-11-30 15:14:26 +0100

    Added LOGOUT

--- app/src/main/java/com/mobiledevices/videoconverter/ui/activities/application/SettingsFragment.kt
+++ app/src/main/java/com/mobiledevices/videoconverter/ui/activities/application/SettingsFragment.kt
@@ -1,11 +1,13 @@
 package com.mobiledevices.videoconverter.ui.activities.application
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mobiledevices.videoconverter.databinding.FragmentSettingsBinding
+import com.mobiledevices.videoconverter.ui.activities.connection.ConnectionActivity
 
 class SettingsFragment : Fragment() {
     private var _binding: FragmentSettingsBinding? = null
@@ -17,6 +19,14 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        binding.btnLogout.setOnClickListener {
+            activity?.finish()
+
+            val intent = Intent(activity, ConnectionActivity::class.java)
+            startActivity(intent)
+        }
+
         return binding.root
     }
 
